100b732cebefc36b27a8457501de9405b4ced437
Rodrigo Linares <rodolinares19@gmail.com>
2018-09-27 01:24:16 -0500

    Add project files.

---
+++ CascadeTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CascadeTest
+{
+    internal class Program
+    {
+        private const string Input = "cells.jpeg";
+        private static readonly CascadeClassifier _cascadeClassifier = new CascadeClassifier("bloodcellhaar.xml");
+
+        private static void Main()
+        {
+            var image = new Image<Bgr, byte>(Input);
+            var gray = image.Convert<Gray, byte>();
+            var cells = _cascadeClassifier.DetectMultiScale(gray);
+
+            foreach (var cell in cells)
+            {
+                image.Draw(cell, new Bgr(0, 0, 1));
+            }
+
+            CvInvoke.Imshow("Result", image);
+            CvInvoke.WaitKey();
+        }
+    }
+}
